d195894bc78f52a00b342bee6ba80c12b8b15566
ansepes <kaz.kaz.613.work@gmail.com>
2021-02-16 17:08:50 +0000

    デプロイ用シェルにconf dir を追加

--- build_source.sh
+++ build_source.sh
@@ -3,7 +3,8 @@
 SOURCE_ZIP="lambda.zip"
 ENTRY_FILE="./lambda_function.py"
 UTILS_DIR="./utils"
+CONFIG_DIR="./conf"
 
 rm -rf $SOURCE_ZIP
 
-zip -r $SOURCE_ZIP $ENTRY_FILE $UTILS_DIR
+zip -r $SOURCE_ZIP $ENTRY_FILE $UTILS_DIR $CONFIG_DIR
